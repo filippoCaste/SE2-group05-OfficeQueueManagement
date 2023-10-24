62ea89b5623d60afbfc5fdb9416e680f3eacc0da
GioGiunta2606 <s318082@studenti.polito.it>
2023-10-24 18:22:34 +0200

    fixed some things in tests but it still not working

--- client/src/tests/unit/components/AppBar.test.js
+++ client/src/tests/unit/components/AppBar.test.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import {render, screen, fireEvent} from '@testing-library/react';
-import {AppNavBar} from '../../../components/AppBar';
+import { MemoryRouter } from 'react-router-dom';
+import AppNavBar from '../../../components/AppBar';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('AppNavBar component', () => {
     test('Render AppNavBar with the correct text', () => {
-        render(<AppNavBar loggedin={false} />);
-        const linkElement = screen.getByText(/Queue Management/i);
+        render(<MemoryRouter>
+                <AppNavBar loggedin={true} handleLogout={() => {}} />
+               </MemoryRouter>);
+        const linkElement = screen.getByText('Queue Management');
         expect(linkElement).toBeInTheDocument();
     });
     
     test('Render AppNavBar with login button', () => {
         render(<AppNavBar loggedin={false} />);
-        const loginButton = screen.getByText(/Login/i);
+        const loginButton = screen.getByText('Login');
         expect(loginButton).toBeInTheDocument();
     });
     
     test('Render AppNavBar with logout button', () => {
         render(<AppNavBar loggedin={true} />);
-        const logoutButton = screen.getByText(/Logout/i);
+        const logoutButton = screen.getByText('Logout');
         expect(logoutButton).toBeInTheDocument();
     });
     
     test('Clicking on "Login" must call the navigate function', () => {
         const mockNavigate = jest.fn();
         render(<AppNavBar loggedin={false} />);
-        const loginButton = screen.getByText(/Login/i);
+        const loginButton = screen.getByText('Login');
         fireEvent.click(loginButton);
         expect(mockNavigate).toHaveBeenCalledTimes(1);
     });
@@ -33,7 +37,7 @@ describe('AppNavBar component', () => {
         const mockNavigate = jest.fn(() => Promise.resolve());
         const mockLogout = jest.fn();
         render(<AppNavBar loggedin={true} handleLogout={mockLogout} />);
-        const logoutButton = screen.getByText(/Logout/i);
+        const logoutButton = screen.getByText('Logout');
         fireEvent.click(logoutButton);
         expect(mockLogout).toHaveBeenCalledTimes(1);
         expect(mockNavigate).toHaveBeenCalledTimes(1);
--- client/src/tests/unit/components/AuthComponents.test.js
+++ client/src/tests/unit/components/AuthComponents.test.js
@@ -1,46 +1,55 @@
 import React from 'react';
 import { render, screen, fireEvent, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import ErrorContext from '../../../errorContext';
 import LoginForm from '../../../components/AuthComponents';
 
-describe('LoginForm Component', () => {
-  test('Renders the login form correctly', () => {
-    render(<LoginForm />);
+describe('LoginForm', () => {
+  it('renders the component correctly', () => {
+    render(
+          <MemoryRouter>
+            <ErrorContext.Provider value={{ handleErrors: jest.fn() }}>
+              <LoginForm login={() => {}} />);
+            </ErrorContext.Provider>
+          </MemoryRouter>);
     
-    const emailInput = screen.getByLabelText('Email');
-    const passwordInput = screen.getByLabelText('Password');
-    const loginButton = screen.getByText('Login');
-    const cancelButton = screen.getByText('Cancel');
-    
-    expect(emailInput).toBeInTheDocument();
-    expect(passwordInput).toBeInTheDocument();
-    expect(loginButton).toBeInTheDocument();
-    expect(cancelButton).toBeInTheDocument();
+    expect(screen.getByText('Login')).toBeInTheDocument();
+    expect(screen.getByLabelText('Email')).toBeInTheDocument();
+    expect(screen.getByLabelText('Password')).toBeInTheDocument();
   });
-  
-  test('Submits the form with the correct data', async () => {
-    const mockLogin = jest.fn().mockResolvedValue({});
-    
+
+  it('submits the form with valid data', async () => {
+    const mockLogin = jest.fn();
     render(<LoginForm login={mockLogin} />);
     
     const emailInput = screen.getByLabelText('Email');
     const passwordInput = screen.getByLabelText('Password');
     const loginButton = screen.getByText('Login');
+
+    userEvent.type(emailInput, 'testuser@example.com');
+    userEvent.type(passwordInput, 'password123');
     
-    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'password123' } });
     fireEvent.click(loginButton);
-    
+
     await act(async () => {
-      expect(mockLogin).toHaveBeenCalledWith({ username: 'test@example.com', password: 'password123' });
+      expect(mockLogin).toHaveBeenCalledWith({ username: 'testuser@example.com', password: 'password123' });
     });
   });
-  
-  test('Handles the "Cancel" button click correctly', () => {
-    const mockNavigate = jest.fn();
-    render(<LoginForm login={() => {}} />);
-    const cancelButton = screen.getByText('Cancel');
-    fireEvent.click(cancelButton);
-    expect(mockNavigate).toHaveBeenCalled();
-    expect(mockNavigate).toHaveBeenCalledWith('/');
+
+  it('displays an error message when form submission fails', async () => {
+    const mockLogin = jest.fn().mockRejectedValue(new Error('Login failed'));
+    const mockHandleErrors = jest.fn();
+    
+    render(<LoginForm login={mockLogin} />);
+    
+    const loginButton = screen.getByText('Login');
+    
+    fireEvent.click(loginButton);
+
+    await act(async () => {
+      expect(mockLogin).toHaveBeenCalled();
+      expect(mockHandleErrors).not.toHaveBeenCalled();
+    });
   });
 });
