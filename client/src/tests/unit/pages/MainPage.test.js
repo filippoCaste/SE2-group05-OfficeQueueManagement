84219c643e30d8adf426f8691fc70b0189d1f990
GioGiunta2606 <s318082@studenti.polito.it>
2023-10-25 17:14:26 +0200

    fixed some tests

--- client/src/tests/unit/pages/MainPage.test.js
+++ client/src/tests/unit/pages/MainPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent, act } from '@testing-library/react';
 import MainPage from '../../../pages/MainPage';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
 
 describe('MainPage Component', () => {
   test('Renders the button to get a ticket', () => {
@@ -13,34 +13,14 @@ describe('MainPage Component', () => {
     expect(getTicketButton).toBeInTheDocument();
   });
 
-  test('Navigates to the Get Ticket page when the button is clicked', () => {
-    const mockNavigate = jest.fn();
-    render(<MemoryRouter>
-              <MainPage navigate={mockNavigate} />
-          </MemoryRouter>);
-    
-    const getTicketButton = screen.getByRole('button');
-    fireEvent.click(getTicketButton);
-    expect(window.location.pathname).toBe('/');
-  });
-
-
-  test('Displays a loading spinner when isLoading is true', () => {
+  test('Navigates to the GetTicket page when the button is clicked', () => {
     render(<MemoryRouter>
               <MainPage />
           </MemoryRouter>);
     
-    const loadingSpinner = screen.getByRole('progressbar');
-    expect(loadingSpinner).toBeNull();
-
-    act(() => {
-      // Set isLoading to true
-      render(<MemoryRouter>
-                  <MainPage />
-              </MemoryRouter>);
-    });
-    
-    const updatedLoadingSpinner = screen.getByRole('progressbar');
-    expect(updatedLoadingSpinner).toBeInTheDocument();
+    const getTicketButton = screen.getByRole('button');
+    fireEvent.click(getTicketButton);
+    expect(window.location.pathname).toHaveBeenCalledWith('/getTicket');
   });
+
 });
